af43e4b1c2ed4a27ca5b94c06c58647e66076a94
Liam Stewart <liamchristopher.media@gmail.com>
2021-03-28 15:22:48 -0700

    adding to index.html

---
+++ Assets/javascript.js
@@ -0,0 +1,5 @@
+// When you click Start Game, the timer will start counting down from 60 and question will prompt.
+// When answer is clicked, question moves on to the next. 
+// If question is answered wrong, decriment time by 10 seconds.
+// If question is answered correct, move to next question.
+// After all questions are answered, prompt input for name and score(time left). This is for high score page.
