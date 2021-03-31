59f84208e669b8a7ab4e0b7fe600a1657d3de73c
Liam Stewart <liamchristopher.media@gmail.com>
2021-03-30 23:42:04 -0700

    local storage code broken. updated readme

--- Assets/javascript.js
+++ Assets/javascript.js
@@ -4,6 +4,11 @@ var container1 = document.getElementById('container1');
 var container2 = document.getElementById('container2');
 var timeEl = document.getElementById('timer');
 var secondsLeft = 60;
+var scoreForm = document.getElementById('score-form');
+var scoreInput = document.getElementById('userName');
+var scoreList = document.getElementById('score-list');
+
+var highScores = [];
 
 
 
@@ -13,7 +18,7 @@ function startGame() {
     var countdown = setInterval(function () {
         secondsLeft--;
         timeEl.textContent = secondsLeft + " seconds left!";
-        if(secondsLeft === 0) {
+        if (secondsLeft === 0) {
             clearInterval(countdown);
             timeEl.textContent = " Time is up! ";
         }
@@ -23,12 +28,12 @@ function startGame() {
 }
 
 function selectAnswer(event) {
-console.log(event.target.dataset.answer)
-// If answer is true or false, hide this question and show the next.
+    console.log(event.target.dataset.answer)
+    // If answer is true or false, hide this question and show the next.
     container1.style.display = "none"
     container2.style.display = "block"
-// If answer is false, decrement time by 20 seconds.
-    if(event.target.dataset.answer === "false") {
+    // If answer is false, decrement time by 20 seconds.
+    if (event.target.dataset.answer === "false") {
         secondsLeft = secondsLeft - 20;
         timeEl.textContent = secondsLeft + " seconds left!";
     }
@@ -39,7 +44,7 @@ console.log(event.target.dataset.answer)
 function selectAnswer2(event) {
     container2.style.display = "none"
     container3.style.display = "block"
-    if(event.target.dataset.answer === "false") {
+    if (event.target.dataset.answer === "false") {
         secondsLeft = secondsLeft - 20;
         timeEl.textContent = secondsLeft + " seconds left!";
     }
@@ -48,7 +53,7 @@ function selectAnswer2(event) {
 function selectAnswer3(event) {
     container3.style.display = "none"
     container4.style.display = "block"
-    if(event.target.dataset.answer === "false") {
+    if (event.target.dataset.answer === "false") {
         secondsLeft = secondsLeft - 20;
         timeEl.textContent = secondsLeft + " seconds left!";
     }
@@ -57,11 +62,16 @@ function selectAnswer3(event) {
 function selectAnswer4(event) {
     container4.style.display = "none"
     window.location.replace("./highscore.html");
-    if(event.target.dataset.answer === "false") {
+    if (event.target.dataset.answer === "false") {
         secondsLeft = secondsLeft - 20;
         timeEl.textContent = secondsLeft + " seconds left!";
     }
 }
+
+
+
+
+
 
 
 //window.location.replace("./highscore.html");
@@ -71,21 +81,33 @@ function selectAnswer4(event) {
 // After all questions are answered, stop timer and prompt input for name and score(time left). This is for high score page.
 
 
-answer4.addEventListener('click', function(event){
-    selectAnswer4(event);
-} )
 
 
-answer3.addEventListener('click', function(event){
-    selectAnswer3(event);
-} )
 
 
-answer2.addEventListener('click', function(event){
-    selectAnswer2(event);
-} )
 
-answer1.addEventListener('click',function(event){
-    selectAnswer(event);
-} )
-startButton.addEventListener('click', startGame);
+
+    answer4.addEventListener('click', function (event) {
+        selectAnswer4(event);
+    })
+
+
+    answer3.addEventListener('click', function (event) {
+        selectAnswer3(event);
+    })
+
+
+    answer2.addEventListener('click', function (event) {
+        selectAnswer2(event);
+    })
+
+    answer1.addEventListener('click', function (event) {
+        selectAnswer(event);
+    })
+    
+    startButton.addEventListener('click', startGame);
+
+    scoreForm.addEventListener("click", function (event) {
+        event.preventDefault();
+    
+    })
